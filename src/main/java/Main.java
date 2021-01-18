5e2ce07c71dc5dd2eebab65b99c3bb58f8ad6b8a
gOni-2247 <57496145+gOni-2247@users.noreply.github.com>
2021-01-18 12:20:25 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
         account.transfer(14537780, "Nome Cognome", "descrizione", "EUR", "10000");
 
-        System.out.println("Questa mofifica e' molto buona");
+        System.out.println("Questa mofifica e' cattiva");
 
 
         
