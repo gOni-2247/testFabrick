bc57126197335315fbfe13ba5b6335f226b8a445
g_oni <giacomobove2247@gmail.com>
2021-01-18 11:09:59 +0100

    modifica feature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,18 +18,9 @@ public class Main {
 
         account.transfer(14537780, "Nome Cognome", "descrizione", "EUR", "10000");
         
-      
-<<<<<<< HEAD
 
         System.out.println("Questa mofifica e' buona");
 
-=======
-<<<<<<< HEAD
-        System.out.println("Questa mofifica e' buona");
-=======
-        System.out.println("Questa mofifica e' cattiva");
->>>>>>> features2
->>>>>>> features2
         
         System.out.println("Aggiungo un altra features qui!");
 
