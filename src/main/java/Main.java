bff4fc9ad114df03cc253348af11bb23f701d0e5
gOni-2247 <57496145+gOni-2247@users.noreply.github.com>
2021-01-18 11:52:14 +0100

    fixing bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
         account.transfer(14537780, "Nome Cognome", "descrizione", "EUR", "10000");
         
-        System.out.println("cacca");
+      
         System.out.println("Questa mofifica");
         
         System.out.println("Aggiungo un altra features qui!");
